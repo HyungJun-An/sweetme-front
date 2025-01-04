2e4d91cafdaca3ca536ae9d6e20d1c0bb9649bad
fhwang98 <fhwang9815@gmail.com>
2025-01-04 13:23:17 +0900

    Feat: 페이지 컴포넌트 및 아이콘 추가

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+// @icons
+
+const Login = () => {
+	return (
+		<section className="px-8">
+			<div className="container mx-auto h-screen grid place-items-center"></div>
+		</section>
+	);
+};
+
+export default Login;
