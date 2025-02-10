5200d21986e87615f0510209a83c201dc90b6712
JongWook6 <jwpark4132@gmail.com>
2025-02-10 21:28:20 +0900

    Feat: 프로필 수정 기능 및 이미지 조회 메서드 추가

--- src/api/profileApi.js
+++ src/api/profileApi.js
@@ -11,8 +11,36 @@ export const getMyProfile = async () => {
   return res.data;
 };
 
-export const updateMyProfile = async (profileData) => {
-  const res = await jwtAxios.put(`${BASE_URL}/profile/me`, profileData);
+export const updateMyProfile = async (profileData, imageFile) => {
+  let config = {};
+  let data;
+
+  if (imageFile) {
+    // 이미지 파일이 있는 경우
+    const formData = new FormData();
+    formData.append('profileData', new Blob([JSON.stringify(profileData)], { type: 'application/json' }));
+    formData.append('file', imageFile);
+
+    config = {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    };
+    data = formData;
+  } else {
+    // 이미지 파일이 없는 경우
+    config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    data = profileData;
+  }
+
+  console.log('headers: ', config);
+  console.log('data: ', data);
+
+  const res = await jwtAxios.put(`${BASE_URL}/profile/me`, data, config);
   return res.data;
 };
 
@@ -21,8 +49,18 @@ export const getOtherProfile = async (profileId) => {
   return res.data;
 };
 
-// TODO: 이미지 조회 API
+// TODO: 이미지 조회 경로
 export const getImageUrl = (path) => {
   if (!path) return null;
-  return `${BASE_URL}/images/${path}`;
+
+  // 이미 완전한 URL 이거나 blob URL 인 경우 그대로 반환
+  if (path.startsWith('http://') || path.startsWith('https://') || path.startsWith('blob:')) {
+    return path;
+  }
+
+  const requestPath = `${BASE_URL}${path}`;
+
+  console.log('requestPath: ', requestPath);
+
+  return requestPath;
 };
